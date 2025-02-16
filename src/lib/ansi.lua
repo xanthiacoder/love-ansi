5278e779b8bdc53b4132b1ce39980eab9048cab5
Joash Chee <joash@scorelessmusic.com>
2025-02-16 22:19:43 +0800

    Added functions for drawing textbox, box, and virtual keyboard with keyboard input

--- src/lib/ansi.lua
+++ src/lib/ansi.lua
@@ -2,8 +2,10 @@
 
 ---------------------------------------------------------------------------------------
 -- Functions:
--- drawBox(x, y, width, height, foreground, background)
 -- drawText(text, x, y, width, fgcolor, bgcolor, fillvalue, fillmax)
+-- drawTextBox(text, x, y, width, height, fgcolor, bgcolor, alignment)
+-- drawBox(title, x, y, width, height, fgcolor, bgcolor, boxtype, fillchar)
+-- drawKeyboard(prompt, case)
 ---------------------------------------------------------------------------------------
 
 -- monotype font is JetBrainsMonoNL-Regular.ttf
@@ -12,12 +14,52 @@
 -- screen 1280 x 720 pixels
 -- 160 x 40 chars
 
+-- Virtual Keyboard Render (41 x 13, max unput length = 39 )
+-- 12345678901234567890123456789012345678901
+local keyboard = {
+[1] = "╔═══════════════════════════════════════╗",
+[2] = "║                                       ║",
+[3] = "╟───╥───╥───╥───╥───╥───╥───╥───╥───╥───╢",
+[4] = "║ 1 ║ 2 ║ 3 ║ 4 ║ 5 ║ 6 ║ 7 ║ 8 ║ 9 ║ 0 ║",
+[5] = "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+[6] = "║ q ║ w ║ e ║ r ║ t ║ y ║ u ║ i ║ o ║ p ║",
+[7] = "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+[8] = "║ - ║ a ║ s ║ d ║ f ║ g ║ h ║ j ║ k ║ l ║",
+[9] = "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╩═══╣",
+[10]= "║ . ║ z ║ x ║ c ║ v ║ b ║ n ║ m ║  del  ║",
+[11]= "╠═══╩═══╩═══╬═══╩═══╩═══╩═══╬═══╩═══════╣",
+[12]= "║ caps lock ║     space     ║   enter   ║",
+[13]= "╚═══════════╩═══════════════╩═══════════╝",
+[14]= "╔═══════════════════════════════════════╗",
+[15]= "║                                       ║",
+[16]= "╟───╥───╥───╥───╥───╥───╥───╥───╥───╥───╢",
+[17]= "║ 1 ║ 2 ║ 3 ║ 4 ║ 5 ║ 6 ║ 7 ║ 8 ║ 9 ║ 0 ║",
+[18]= "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+[19]= "║ Q ║ W ║ E ║ R ║ T ║ Y ║ U ║ I ║ O ║ P ║",
+[20]= "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣",
+[21]= "║ - ║ A ║ S ║ D ║ F ║ G ║ H ║ J ║ K ║ L ║",
+[22]= "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╩═══╣",
+[23]= "║ . ║ Z ║ X ║ C ║ V ║ B ║ N ║ M ║  del  ║",
+[24]= "╠═══╩═══╩═══╬═══╩═══╩═══╩═══╬═══╩═══════╣",
+[25]= "║ caps lock ║     space     ║   enter   ║",
+[26]= "╚═══════════╩═══════════════╩═══════════╝"
+}
+
+
+local TEXT_WIDTH = "0---------1---------2---------3---------4---------5---------6---------7---------8---------9---------0---------1---------2---------3---------4---------5---------"
+local TEXT_HEIGHT = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n"
+local TEXT_BLOCKS = "▁ ▂ ▃ ▄ ▅ ▆ ▇ █ ▀ ▔ ▏ ▎ ▍ ▌ ▋ ▊ ▉ ▐ ▕ ▖ ▗ ▘ ▙ ▚ ▛ ▜ ▝ ▞ ▟ ░ ▒ ▓ "
+local TEXT_CIRCLES = "● ○ ◯ ◔ ◕ ◶ ◌ ◉ ◎ ◦ "
+local TEXT_SQUARES = "◆ ◇ ◈ ◊ ■ □ ▪ ▫ ◧ ◨ ◩ ◪ ◫ "
+local TEXT_TRIANGLES = "▲ ▶ ▼ ◀ △ ▷ ▽ ◁ ► ◄ ▻ ◅ ▴ ▸ ▾ ◂ ▵ ▹ ▿ ◃ "
+local TEXT_BOXES = "╦ ╗ ╔ ═ ╩ ╝ ╚ ║ ╬ ╣ ╠ ╥ ╖ ╓ ┰ ┒ ┧ ┎ ┟ ╁ ┯ ┑ ┩ ┍ ┡ ╇ ╤ ╕ ╒ ╍ ╏ ╻ ┳ ┓ ┏ ━ ╸ ╾ ┉ ┋ ╺ ┅ ┇ ╹ ┻ ┛ ╿ ┗ ┃ ╋ ┫ ┣ ╅ ┭ ┵ ┽ ┲ ┺ ╊ ╃ ╮ ╭ ╯ ╰ ╳ ╲ ╱ ╌ ╎ ╷ ┬ ┐ ┌ ─ ╴ ╼ ┈ ┊ ╶ ┄ ┆ ╵ ╽ ┴ ┘ └ │ ┼ ┤ ├ ╆ ┮ ┶ ┾ ┱ ┹ ╉ ╄ ╨ ╜ ╙ ╀ ┸ ┦ ┚ ┞ ┖ ╈ ┷ ┪ ┙ ┢ ┕ ╧ ╛ ╘ ╫ ╢ ╟ ╂ ┨ ┠ ┿ ┥ ┝ ╪ ╡ ╞ "
+
 FONT = "JetBrainsMonoNL-Regular.ttf"
 FONT_SIZE = 14
 FONT_WIDTH = 8
 FONT_HEIGHT = 18
 
---	eg. 	love.graphics.setColor( color.white )
+--	eg. 	love.graphics.setColor( color.white ) or color[0..15]
 color = {
 	black 			= {   0,   0,   0, 1 }, [0] 			= {   0,   0,   0, 1 },
 	red 			= { 0.5,   0,   0, 1 },	[1] 			= { 0.5,   0,   0, 1 },
@@ -39,35 +81,17 @@ color = {
 	white 			= {   1,   1,   1, 1 },	[15] 			= {   1,   1,   1, 1 },
 	}
 
-function drawBox(x, y, width, height, foreground, background)
-
-	local i = 0
-	love.graphics.setColor( background )
-	love.graphics.rectangle("fill", x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, height*FONT_HEIGHT)
-	love.graphics.setColor( foreground )
-	love.graphics.printf("┌", x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-	love.graphics.printf("╘", x*FONT_WIDTH, (y+height-1)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-	love.graphics.printf("╖", (x+width-1)*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-	love.graphics.printf("╝", (x+width-1)*FONT_WIDTH, (y+height-1)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-	for i = 1,width-2 do
-		love.graphics.printf("─", (x+i)*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-		love.graphics.printf("═", (x+i)*FONT_WIDTH, (height+y-1)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-	end
-	for i = 1,height-2 do
-		love.graphics.printf("│", x*FONT_WIDTH, (y+i)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-		love.graphics.printf("║", (x+width-1)*FONT_WIDTH, (y+i)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
-	end
-end -- drawBox
 
 
 function drawText(text, x, y, width, fgcolor, bgcolor, fillvalue, fillmax)
 
-	-- draw background first using thick line
-	love.graphics.setLineWidth( FONT_HEIGHT )
-	-- draw background's background first with fillmax, alpha / 2
-	love.graphics.setColor( bgcolor[1], bgcolor[2], bgcolor[3], bgcolor[4]/2 )
+	-- draw background's background first with fillmax, half all RGB
+	-- draw background's background using rectangle
+	love.graphics.setColor( bgcolor[1]/2, bgcolor[2]/2, bgcolor[3]/2, bgcolor[4] )
 	love.graphics.rectangle("fill", x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, FONT_HEIGHT)
 	-- draw background's fill based on fillvalue/fillmax
+	-- draw background's fill using thick line
+	love.graphics.setLineWidth( FONT_HEIGHT )
 	love.graphics.setColor( bgcolor )
 	love.graphics.line(x*FONT_WIDTH, (y*FONT_HEIGHT)+(FONT_HEIGHT/2), (x*FONT_WIDTH)+((width*FONT_WIDTH)*(fillvalue/fillmax)), (y*FONT_HEIGHT)+(FONT_HEIGHT/2))
 
@@ -76,3 +100,143 @@ function drawText(text, x, y, width, fgcolor, bgcolor, fillvalue, fillmax)
 	love.graphics.printf(string.sub(text, 1, width), x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, 'left')
 
 end -- drawText
+
+
+function drawTextBox(text, x, y, width, height, fgcolor, bgcolor, alignment)
+
+	-- draw background using rectangle
+	love.graphics.setColor( bgcolor )
+	love.graphics.rectangle("fill", x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, height*FONT_HEIGHT)
+
+	-- draw foreground text
+	love.graphics.setColor( fgcolor )
+	love.graphics.printf(text, x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, alignment)
+
+end -- drawTextBox
+
+
+-- ansi.box("boxtitle", x, y, width, height, fgcolor, bgcolor, type, fill)
+-- type = single, double, rounded, thick, borderless
+-- fill = single char to fill the box
+function drawBox(title, x, y, width, height, fgcolor, bgcolor, boxtype, fillchar)
+
+	-- draw background using rectangle
+	love.graphics.setColor( bgcolor )
+	love.graphics.rectangle("fill", x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, height*FONT_HEIGHT)
+
+	-- draw foreground box
+	-- set box glyphs according to boxtype
+
+	local topLeft, topRight, bottomLeft, bottomRight, horizontal, vertical = ""
+	if boxtype == "single" then
+		topLeft = "┌"
+		topRight = "┐"
+		bottomLeft = "└"
+		bottomRight = "┘"
+		horizontal = "─"
+		vertical = "│"
+	elseif boxtype == "double" then
+		topLeft = "╔"
+		topRight = "╗"
+		bottomLeft = "╚"
+		bottomRight = "╝"
+		horizontal = "═"
+		vertical = "║"	
+	elseif boxtype == "rounded" then
+		topLeft = "╭"
+		topRight = "╮"
+		bottomLeft = "╰"
+		bottomRight = "╯"
+		horizontal = "─"
+		vertical = "│"
+	elseif boxtype == "thick" then
+		topLeft = "┏"
+		topRight = "┓"
+		bottomLeft = "┗"
+		bottomRight = "┛"
+		horizontal = "━"
+		vertical = "┃"	
+	elseif boxtype == "borderless" then
+		topLeft = " "
+		topRight = " "
+		bottomLeft = " "
+		bottomRight = " "
+		horizontal = " "
+		vertical = " "
+	end
+
+	love.graphics.setColor( fgcolor )
+	-- draw corners
+	love.graphics.printf(topLeft, x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+	love.graphics.printf(bottomLeft, x*FONT_WIDTH, (y+height-1)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+	love.graphics.printf(topRight, (x+width-1)*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+	love.graphics.printf(bottomRight, (x+width-1)*FONT_WIDTH, (y+height-1)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+	-- draw horizontal lines
+	local i = 0
+	for i = 1,width-2 do
+		love.graphics.printf(horizontal, (x+i)*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+		love.graphics.printf(horizontal, (x+i)*FONT_WIDTH, (height+y-1)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+	end
+	-- draw vertical lines
+	for i = 1,height-2 do
+		love.graphics.printf(vertical, x*FONT_WIDTH, (y+i)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+		love.graphics.printf(vertical, (x+width-1)*FONT_WIDTH, (y+i)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+	end
+
+	-- draw fill char
+	local j = 0
+	for i = 1,height-2 do
+		for j = 1,width-2 do
+			love.graphics.printf(fillchar, (x+j)*FONT_WIDTH, (y+i)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+		end -- j
+	end -- i
+
+	-- draw title if it's not blank
+	if title ~= "" then
+		drawText("["..title.."]", x+2, y, string.len(title)+2, fgcolor, bgcolor, 1, 1)
+	end
+end -- drawBox
+
+-- virtual keyboard (for console compatibility)
+-- uses global from main.lua : game.keyboard.input
+-- fixed x, y, width = 41, height = 13, fgcolor, bgcolor, maxinputlength = width-2, case = "upper" or "lower"
+function drawKeyboard(prompt, case)
+
+	local dataEntry = ""
+	local dataSubmit = false
+	local x = 59
+	local y = 13
+	local width = 41
+	local height = 13
+	local fgcolor = color.white
+	local bgcolor = color.darkgray
+
+	-- draw background using rectangle
+	love.graphics.setColor( bgcolor )
+	love.graphics.rectangle("fill", x*FONT_WIDTH, y*FONT_HEIGHT, width*FONT_WIDTH, height*FONT_HEIGHT)
+
+	-- draw current selected key (using global in main.lua game.keyboard.selectx and selecty)
+	drawTextBox("▗▄▄▄▖\n▐███▍\n▝▀▀▀▘", x+((game.keyboard.selectx-1)*4), y+2+((game.keyboard.selecty-1)*2), 5, 3, color.brightblue, color.darkgray, "left")	
+
+	-- draw upper or lower case keyboard
+	local i = 0
+	love.graphics.setColor( fgcolor )
+	if case == "lower" then
+		for i = 0,12 do
+			love.graphics.printf(keyboard[i+1], x*FONT_WIDTH, (y+i)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+		end
+	elseif case == "upper" then
+		for i = 0,12 do
+			love.graphics.printf(keyboard[i+14], x*FONT_WIDTH, (y+i)*FONT_HEIGHT, width*FONT_WIDTH, 'left')
+		end	
+	end
+
+	-- draw prompt if it's not blank
+	if prompt ~= "" then
+		drawText("["..prompt.."]", x+2, y, string.len(prompt)+2, fgcolor, bgcolor, 1, 1)
+	end
+
+	-- draw game.keyboard.input text
+	drawText(game.keyboard.input, x+1, y+1, width-2, color.brightyellow, color.brightblue, string.len(game.keyboard.input), 39)
+
+end -- inputKeyboard
--- src/main.lua
+++ src/main.lua
@@ -10,16 +10,131 @@ love.filesystem.setIdentity("love-ansi") -- for R36S file system compatibility
 
 require "lib.ansi"
 
+-- lookup table for virtual keyboard ([y][x])
+vkey = {}
+vkey[1] = {
+[1] = "1",
+[2] = "2",
+[3] = "3",
+[4] = "4",
+[5] = "5",
+[6] = "6",
+[7] = "7",
+[8] = "8",
+[9] = "9",
+[10]= "0"
+}
+vkey[2] = {
+[1] = "q",
+[2] = "w",
+[3] = "e",
+[4] = "r",
+[5] = "t",
+[6] = "y",
+[7] = "u",
+[8] = "i",
+[9] = "o",
+[10]= "p"
+}
+vkey[3] = {
+[1] = "-",
+[2] = "a",
+[3] = "s",
+[4] = "d",
+[5] = "f",
+[6] = "g",
+[7] = "h",
+[8] = "j",
+[9] = "k",
+[10]= "l"
+}
+vkey[4] = {
+[1] = ".",
+[2] = "z",
+[3] = "x",
+[4] = "c",
+[5] = "v",
+[6] = "b",
+[7] = "n",
+[8] = "m",
+[9] = "",
+[10]= ""
+}
+vkey[5] = {
+[1] = "",
+[2] = "",
+[3] = "",
+[4] = " ",
+[5] = " ",
+[6] = " ",
+[7] = " ",
+[8] = "",
+[9] = "",
+[10]= ""
+}
+vkey[6] = {
+[1] = "1",
+[2] = "2",
+[3] = "3",
+[4] = "4",
+[5] = "5",
+[6] = "6",
+[7] = "7",
+[8] = "8",
+[9] = "9",
+[10]= "0"
+}
+vkey[7] = {
+[1] = "Q",
+[2] = "W",
+[3] = "E",
+[4] = "R",
+[5] = "T",
+[6] = "Y",
+[7] = "U",
+[8] = "I",
+[9] = "O",
+[10]= "P"
+}
+vkey[8] = {
+[1] = "-",
+[2] = "A",
+[3] = "S",
+[4] = "D",
+[5] = "F",
+[6] = "G",
+[7] = "H",
+[8] = "J",
+[9] = "K",
+[10]= "L"
+}
+vkey[9] = {
+[1] = ".",
+[2] = "Z",
+[3] = "X",
+[4] = "C",
+[5] = "V",
+[6] = "B",
+[7] = "N",
+[8] = "M",
+[9] = "",
+[10]= ""
+}
+vkey[10] = {
+[1] = "",
+[2] = "",
+[3] = "",
+[4] = " ",
+[5] = " ",
+[6] = " ",
+[7] = " ",
+[8] = "",
+[9] = "",
+[10]= ""
+}
+
+
 -- define global variables used in all scenes
-
-TEXT_WIDTH = "0---------1---------2---------3---------4---------5---------6---------7---------8---------9---------0---------1---------2---------3---------4---------5---------"
-TEXT_HEIGHT = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n"
-TEXT_BLOCKS = "▁ ▂ ▃ ▄ ▅ ▆ ▇ █ ▀ ▔ ▏ ▎ ▍ ▌ ▋ ▊ ▉ ▐ ▕ ▖ ▗ ▘ ▙ ▚ ▛ ▜ ▝ ▞ ▟ ░ ▒ ▓ "
-TEXT_CIRCLES = "● ○ ◯ ◔ ◕ ◶ ◌ ◉ ◎ ◦ "
-TEXT_SQUARES = "◆ ◇ ◈ ◊ ■ □ ▪ ▫ ◧ ◨ ◩ ◪ ◫ "
-TEXT_TRIANGLES = "▲ ▶ ▼ ◀ △ ▷ ▽ ◁ ► ◄ ▻ ◅ ▴ ▸ ▾ ◂ ▵ ▹ ▿ ◃ "
-TEXT_BOXES = "╦ ╗ ╔ ═ ╩ ╝ ╚ ║ ╬ ╣ ╠ ╥ ╖ ╓ ┰ ┒ ┧ ┎ ┟ ╁ ┯ ┑ ┩ ┍ ┡ ╇ ╤ ╕ ╒ ╍ ╏ ╻ ┳ ┓ ┏ ━ ╸ ╾ ┉ ┋ ╺ ┅ ┇ ╹ ┻ ┛ ╿ ┗ ┃ ╋ ┫ ┣ ╅ ┭ ┵ ┽ ┲ ┺ ╊ ╃ ╮ ╭ ╯ ╰ ╳ ╲ ╱ ╌ ╎ ╷ ┬ ┐ ┌ ─ ╴ ╼ ┈ ┊ ╶ ┄ ┆ ╵ ╽ ┴ ┘ └ │ ┼ ┤ ├ ╆ ┮ ┶ ┾ ┱ ┹ ╉ ╄ ╨ ╜ ╙ ╀ ┸ ┦ ┚ ┞ ┖ ╈ ┷ ┪ ┙ ┢ ┕ ╧ ╛ ╘ ╫ ╢ ╟ ╂ ┨ ┠ ┿ ┥ ┝ ╪ ╡ ╞ "
-
 
 game = {}
 game.power = {}
@@ -31,6 +146,16 @@ width, height = love.graphics.getDimensions( )
 game.width = width
 game.height = height
 
+-- game states
+
+game.keyboard = {
+	show = false,
+	prompt = "",
+	case = "lower",
+	input = "",
+	selectx = 1,
+	selecty = 1,
+}
 
 -- one-time setup of game / app, loading assets
 function love.load()
@@ -47,6 +172,61 @@ function love.keypressed(key, scancode, isrepeat)
 		love.window.setFullscreen(fullscreen, "exclusive")
 	end
 end
+
+
+-- this function switching all input methods for keyboard entry
+-- fixed width = 39 (max string length)
+function inputKeyboard()
+
+	local width = 39
+	
+	function love.keypressed(key, scancode, isrepeat)
+		if scancode == "up" and game.keyboard.selecty ~= 1 then
+			game.keyboard.selecty = game.keyboard.selecty - 1
+		elseif scancode == "left" and game.keyboard.selectx ~= 1 then
+			game.keyboard.selectx = game.keyboard.selectx - 1
+		elseif scancode == "down" and game.keyboard.selecty ~= 5 then
+			game.keyboard.selecty = game.keyboard.selecty + 1
+		elseif scancode == "right" and game.keyboard.selectx ~= 10 then
+			game.keyboard.selectx = game.keyboard.selectx + 1
+		end
+
+		-- when return is pressed
+		if scancode == "return" then
+			if game.keyboard.case == "lower" and string.len(game.keyboard.input) < width then
+				game.keyboard.input = game.keyboard.input .. vkey[game.keyboard.selecty][game.keyboard.selectx]
+			elseif game.keyboard.case == "upper" and string.len(game.keyboard.input) < width then
+				game.keyboard.input = game.keyboard.input .. vkey[game.keyboard.selecty+5][game.keyboard.selectx]
+			end
+			if game.keyboard.selecty == 4 and (game.keyboard.selectx == 9 or game.keyboard.selectx == 10) then
+			-- delete entered
+				game.keyboard.input = game.keyboard.input:sub(1, -2)
+			end
+			if game.keyboard.selecty == 5 then
+				if game.keyboard.selectx <= 3 then -- caps lock entered
+					if game.keyboard.case == "lower" then
+						game.keyboard.case = "upper"
+					else
+						game.keyboard.case = "lower"
+					end
+				elseif game.keyboard.selectx <= 7 then -- spacebar entered
+					-- do spacebar stuff
+				else -- return entered
+					game.keyboard.show = false
+				end
+			end		
+		end -- scancode == "return"
+
+	end
+	
+end
+
+
+-- sequential commands for testing
+game.keyboard.show = true
+game.keyboard.input = ""
+inputKeyboard()
+
 
 -- callback for graceful exit
 function love.quit()
@@ -66,68 +246,11 @@ function love.draw()
 --	love.graphics.printf(TEXT_WIDTH, monoFont, 0, 0, game.width, "left")
 --	love.graphics.printf(TEXT_HEIGHT, monoFont, 0, 0, game.width, "left")
 
-	love.graphics.setColor( color.brightblue )
-	love.graphics.printf(TEXT_BLOCKS .. TEXT_CIRCLES .. TEXT_SQUARES .. TEXT_TRIANGLES .. TEXT_BOXES, monoFont, 0, 0, game.width, "left")
+	drawBox("", 10, 10, 40, 5, color.white, color.green, "rounded", "░")
 
-	love.graphics.setColor( color[1] )
-	love.graphics.printf("█████████", monoFont, 0, 18*10, game.width, "left")
-	love.graphics.setColor( color[2] )
-	love.graphics.printf("█ Joash █", monoFont, 0, 18*11, game.width, "left")
-	love.graphics.setColor( color[3] )
-	love.graphics.printf("█ CHEE  █", monoFont, 0, 18*12, game.width, "left")
-	love.graphics.setColor( color[4] )
-	love.graphics.printf("█████████", monoFont, 0, 18*13, game.width, "left")
-	love.graphics.setColor( color[5] )
-	love.graphics.printf("│││││││││\n│", monoFont, 0, 18*14, game.width, "left")
-
-local xsize = 8
-
-	love.graphics.setColor( color[6] )
-	love.graphics.printf("│", monoFont, xsize*1, 18*15, game.width, "left")
-	love.graphics.setColor( color[7] )
-	love.graphics.printf("│", monoFont, xsize*2, 18*15, game.width, "left")
-	love.graphics.setColor( color[8] )
-	love.graphics.printf("│", monoFont, xsize*3, 18*15, game.width, "left")
-	love.graphics.setColor( color[9] )
-	love.graphics.printf("│", monoFont, xsize*4, 18*15, game.width, "left")
-	love.graphics.setColor( color[10] )
-	love.graphics.printf("│", monoFont, xsize*5, 18*15, game.width, "left")
-
-	love.graphics.setColor( color.red )
-	love.graphics.printf("red", monoFont, 0, FONT_HEIGHT*16, game.width, "left")
-	love.graphics.setColor( color.green )
-	love.graphics.printf("green", monoFont, 0, FONT_HEIGHT*17, game.width, "left")
-	love.graphics.setColor( color.yellow )
-	love.graphics.printf("yellow", monoFont, 0, FONT_HEIGHT*18, game.width, "left")
-	love.graphics.setColor( color.blue )
-	love.graphics.printf("blue", monoFont, 0, FONT_HEIGHT*19, game.width, "left")
-	love.graphics.setColor( color.magenta )
-	love.graphics.printf("magenta", monoFont, 0, FONT_HEIGHT*20, game.width, "left")
-	love.graphics.setColor( color.cyan )
-	love.graphics.printf("cyan", monoFont, 0, FONT_HEIGHT*21, game.width, "left")
-	love.graphics.setColor( color.gray )
-	love.graphics.printf("gray", monoFont, 0, FONT_HEIGHT*22, game.width, "left")
-	love.graphics.setColor( color.darkgray )
-	love.graphics.printf("dark gray", monoFont, 0, FONT_HEIGHT*23, game.width, "left")
-	love.graphics.setColor( color.brightred )
-	love.graphics.printf("bright red", monoFont, 0, FONT_HEIGHT*24, game.width, "left")
-	love.graphics.setColor( color.brightgreen )
-	love.graphics.printf("bright green", monoFont, 0, FONT_HEIGHT*25, game.width, "left")
-	love.graphics.setColor( color.brightyellow )
-	love.graphics.printf("bright yellow", monoFont, 0, FONT_HEIGHT*26, game.width, "left")
-	love.graphics.setColor( color.brightblue )
-	love.graphics.printf("bright blue", monoFont, 0, FONT_HEIGHT*27, game.width, "left")
-	love.graphics.setColor( color.brightmagenta )
-	love.graphics.printf("bright magenta", monoFont, 0, FONT_HEIGHT*28, game.width, "left")
-	love.graphics.setColor( color.brightcyan )
-	love.graphics.printf("bright cyan", monoFont, 0, FONT_HEIGHT*29, game.width, "left")
-	love.graphics.setColor( color.white )
-	love.graphics.printf("white", monoFont, 0, FONT_HEIGHT*30, game.width, "left")
-
-	drawText(" Haste", 100, 20, 20, color.black, color.brightyellow, 45, 60)
-	drawText(" Blur ", 100, 21, 20, color.black, color.brightyellow, 30, 120)
-	drawText(" Flying", 100, 22, 20, color.brightcyan, color.brightblue, 1, 1)
-
+	if game.keyboard.show then
+		drawKeyboard(game.keyboard.prompt, game.keyboard.case)
+	end
 
 	-- display game tooltip
 --	love.graphics.printf(math.floor(game.height/(game.width/16)), smallFont, 0, 0, game.width, "left")
